d0d496540bd803372ddb8219a059cd073d70c3aa
Klaus Nordhausen <klaus.nordhausen@uta.fi>
2008-09-14 00:00:00 +0000

    version 1.1-3

--- R/mean3.R
+++ R/mean3.R
@@ -8,7 +8,9 @@ mean3 <- function(X, na.action = na.fail)
     if(!all(sapply(X, is.numeric))) stop("'X' must be numeric")
     p <- dim(X)[2]
     if (p<2) stop("'X' must be at least bivariate")
-    r.i.2 <-  mahalanobis(X, colMeans(X), cov(X))
+    n <- dim(X)[1]
+    cor.fac <- (n-1)/n
+    r.i.2 <- mahalanobis(X, colMeans(X), cor.fac*cov(X))
     Y <- sweep(X,1,r.i.2,"*")
     colMeans(Y)/p
     }

57f3a77d6d9f9273d9bba616b05b16ea2daa9541
Andreas Alfons <alfons@ese.eur.nl>
2023-03-03 18:52:58 +0100

    updated argument checks and moved computation of S2_Y back to function ICS()

--- R/ICS_S3.R
+++ R/ICS_S3.R
@@ -71,96 +71,96 @@ ICS_covW <- function(x, na.action = na.fail, alpha = 1, cf = 1 ) {
 }
 
 
-S2_Y <- function(X, S1, S2, ...) UseMethod("S2_Y", S2)
-
-S2_Y.function <- function(X, S1.X, S2, S2_args = list(), QR = FALSE,
-                          whiten = !QR, S1_label, S2_label, ...) {
-  # Initialization
-  S2.X <- S2
-
-  if (QR) {
-
-    # Details
-    n <- nrow(X)
-    p <- ncol(X)
-
-    # perform algorithm based on QR decomposition
-    # centering by the mean
-    X.c = sweep(X, 2, colMeans(X), "-")
-
-    # Permutation by rows
-    # decreasing by infinity norm:  absolute maximum
-    norm_inf <- apply(X.c, 1, function(x) max(abs(x)))
-    order_rows <- order(norm_inf, decreasing = TRUE)
-    X_row_per <- X.c[order_rows,]
-
-    # QR decomposition of X with column pivoting from LAPACK
-    qr_X <- qr( 1/sqrt(n-1)*X_row_per, LAPACK = TRUE)
-
-    # R should be pxp
-    R <- qr.R(qr_X)
-
-    # Q should be nxp
-    Q <- qr.Q(qr_X)
-
-    # computation of d_i
-    d_i <- (n-1)*apply(Q, 1, function(x) sum(x^2))
-
-    # Spectral decomposition (SEP) of S2.Y
-    # Computation of S2 - with general one-step M-estimators
-    S2.Y <- S2_args$cf*1/n*(n-1)*t(Q) %*% diag(d_i^S2_args$alpha) %*% Q
-    # convert scatter matrices to class "ICS_scatter"
-    S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
-
-    # Reorder the rows and cols of X
-    X_Z <-  X_row_per[order(order_rows),qr_X$pivot]
-    B1 <- NULL
-
-  } else {
-
-    # compute B1 = S1.X^-1/2
-    S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
-    B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
-
-    # obtain second scatter matrix and whiten the data if requested
-    # convert scatter matrices to class "ICS_scatter"
-    if (whiten) {
-      Y <- X %*% B1
-      S2.Y <- get_scatter(Y, fun = S2, args = S2_args, label = S2_label)
-    } else {
-      # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
-      S2.X <- get_scatter(X, fun = S2, args = S2_args)
-      S2.Y <- to_ICS_scatter(B1 %*% S2.X$scatter %*% B1, label = S2_label)
-    }
-    X_Z <- X
-    R <- NULL
-
-  }
-
-  # return list of results
-  list(X, S2.X = S2.X, S2.Y = S2.Y, B1 = B1, R = R, X_Z = X_Z, ...)
-}
-
-
-S2_Y.matrix <- function(X, S1.X, S2, S1_label, S2_label, ...) {
-
-  # perform 1st step of ICS
-  # compute B1 = S1.X^-1/2
-  S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
-  B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
-
-  # obtain second scatter matrix
-  # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
-  # convert scatter matrices to class "ICS_scatter"
-  S2.X <- to_ICS_scatter(S2, label = S2_label)
-  S2.Y <- B1 %*% S2.X$scatter %*% B1
-  S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
-
-  # return list of results
-  list(S2.X = S2.X, S2.Y = S2.Y, B1 = B1, X_Z = X, ...)
-}
-
-S2_Y.ICS_scatter <- S2_Y.matrix
+# S2_Y <- function(X, S1, S2, ...) UseMethod("S2_Y", S2)
+#
+# S2_Y.function <- function(X, S1.X, S2, S2_args = list(), QR = FALSE,
+#                           whiten = !QR, S1_label, S2_label, ...) {
+#   # Initialization
+#   S2.X <- S2
+#
+#   if (QR) {
+#
+#     # Details
+#     n <- nrow(X)
+#     p <- ncol(X)
+#
+#     # perform algorithm based on QR decomposition
+#     # centering by the mean
+#     X.c = sweep(X, 2, colMeans(X), "-")
+#
+#     # Permutation by rows
+#     # decreasing by infinity norm:  absolute maximum
+#     norm_inf <- apply(X.c, 1, function(x) max(abs(x)))
+#     order_rows <- order(norm_inf, decreasing = TRUE)
+#     X_row_per <- X.c[order_rows,]
+#
+#     # QR decomposition of X with column pivoting from LAPACK
+#     qr_X <- qr( 1/sqrt(n-1)*X_row_per, LAPACK = TRUE)
+#
+#     # R should be pxp
+#     R <- qr.R(qr_X)
+#
+#     # Q should be nxp
+#     Q <- qr.Q(qr_X)
+#
+#     # computation of d_i
+#     d_i <- (n-1)*apply(Q, 1, function(x) sum(x^2))
+#
+#     # Spectral decomposition (SEP) of S2.Y
+#     # Computation of S2 - with general one-step M-estimators
+#     S2.Y <- S2_args$cf*1/n*(n-1)*t(Q) %*% diag(d_i^S2_args$alpha) %*% Q
+#     # convert scatter matrices to class "ICS_scatter"
+#     S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
+#
+#     # Reorder the rows and cols of X
+#     X_Z <-  X_row_per[order(order_rows),qr_X$pivot]
+#     B1 <- NULL
+#
+#   } else {
+#
+#     # compute B1 = S1.X^-1/2
+#     S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
+#     B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
+#
+#     # obtain second scatter matrix and whiten the data if requested
+#     # convert scatter matrices to class "ICS_scatter"
+#     if (whiten) {
+#       Y <- X %*% B1
+#       S2.Y <- get_scatter(Y, fun = S2, args = S2_args, label = S2_label)
+#     } else {
+#       # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
+#       S2.X <- get_scatter(X, fun = S2, args = S2_args)
+#       S2.Y <- to_ICS_scatter(B1 %*% S2.X$scatter %*% B1, label = S2_label)
+#     }
+#     X_Z <- X
+#     R <- NULL
+#
+#   }
+#
+#   # return list of results
+#   list(X, S2.X = S2.X, S2.Y = S2.Y, B1 = B1, R = R, X_Z = X_Z, ...)
+# }
+#
+#
+# S2_Y.matrix <- function(X, S1.X, S2, S1_label, S2_label, ...) {
+#
+#   # perform 1st step of ICS
+#   # compute B1 = S1.X^-1/2
+#   S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
+#   B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
+#
+#   # obtain second scatter matrix
+#   # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
+#   # convert scatter matrices to class "ICS_scatter"
+#   S2.X <- to_ICS_scatter(S2, label = S2_label)
+#   S2.Y <- B1 %*% S2.X$scatter %*% B1
+#   S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
+#
+#   # return list of results
+#   list(S2.X = S2.X, S2.Y = S2.Y, B1 = B1, X_Z = X, ...)
+# }
+#
+# S2_Y.ICS_scatter <- S2_Y.matrix
 
 
 
@@ -187,71 +187,179 @@ S2_Y.ICS_scatter <- S2_Y.matrix
 #' @examples
 ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
                 S2_args =  list(), QR = NULL,  whiten = NULL,
-                center = FALSE, fix_signs = c("scores", "W"),
-                scale_lambda = FALSE, na.action = na.fail, ...) {
+                center = FALSE, scale_lambda = FALSE,
+                fix_signs = c("scores", "W"),
+                na.action = na.fail, ...) {
 
-  # initializations - data matrix
+  # make sure we have a suitable data matrix
   X <- na.action(X)
   X <- as.matrix(X)
   p <- ncol(X)
-  if (p < 2) stop("'X' must be at least bivariate")
+  if (p < 2L) stop("'X' must be at least bivariate")
 
-  # obtain first scatter matrix
+  # obtain default labels for scatter matrices
   S1_label <- deparse(substitute(S1))
   S2_label <- deparse(substitute(S2))
 
-  # convert scatter matrices to class "ICS_scatter"
-  if (is.function(S1)) {
-    S1.X <- get_scatter(X, fun = S1, args = S1_args, label = S1_label)
-  } else S1.X <- to_ICS_scatter(S1, label = S1_label)
-  # TODO: I think we need to update S1_label here to S1.X$label
-<<<<<<< HEAD
-  #       (then the check below for the default value of 'QR'
-  #       may need to be adjusted accordingly)
-  # Aurore: yes
-=======
->>>>>>> f526dc92040b6c4c188186b14987ca6d9d17f717
+  # check argument for QR algorithm
+  have_QR <- !is.null(QR)
+  if (have_QR) QR <- isTRUE(QR)
+  # QR algorithm requires a certain class of scatter pairs supplied as functions
+  have_cov <- identical(S1, cov) || identical(S1, ICS_cov)
+  have_covW <- identical(S2, covW) || identical(S2, ICS_covW)
+  if (have_cov && have_covW) {
+    # use QR algorithm by default
+    if (!have_QR) QR <- TRUE
+  } else {
+    # QR algorithm not applicable for other scatter pairs
+    if (have_QR && QR) {
+      warning("QR algorithm is not applicable; proceeding without it")
+    }
+    QR <- FALSE
+  }
 
-  # if S2 is a function, we need to set some default values
+  # check argument for whitening
+  have_whiten <- !is.null(whiten)
+  if (have_whiten) whiten <- isTRUE(whiten)
+  # whitening requires S2 to be a function
   if (is.function(S2)) {
-    # QR algorithm can only be applied for a certain class of scatter pairs
-    # TODO: we should check whether we have the actual functions rather than
-    #       checking the labels, no? I can submit a scatter matrix named "cov"
-    #       (no matter how it was computed) and it would pass the check.
-    if (S1_label %in% c("cov", "ICS_cov") && S2_label %in% c("covW", "ICS_covW")) {
-      if (is.null(QR)) QR <- TRUE  # use QR algorithm by default
-      else QR <- isTRUE(QR)        # otherwise ensure correct user input
-    } else QR <- FALSE             # not applicable for other scatter pairs
-    # whitening is only relevant when we don't have the QR algorithm
-    if (QR) whiten <- FALSE
-    else {
-      # use whitening by default, otherwise ensure correct user input
-      if (is.null(whiten)) whiten <- TRUE
-      else whiten <- isTRUE(whiten)
+    if (QR) {
+      # whitening is not applicable when we have the QR algorithm
+      warning("whitening not applicable for QR algorithm; ",
+              "proceeding without whitening")
+      whiten <- FALSE
+    } else if (!have_whiten) {
+      # use whitening by default
+      whiten <- TRUE
     }
   } else {
-    # when S2 is a scatter matrix, QR algorithm or whitening are not applicable
-    # TODO: should we add some warnings when we change the default values?
-    QR <- FALSE
+    # whitening not applicable
+    if (have_whiten && whiten) {
+      warning("whitening requires 'S2' to be a function; ",
+              "proceeding without whitening")
+    }
     whiten <- FALSE
   }
 
-  # compute S2_Y
-  S2_Y <- S2_Y(X, S1.X = S1.X, S2 = S2, S2_args = S2_args,
-               QR = QR, whiten = whiten, S1_label, S2_label, ... )
-  X_Z <- S2_Y$X_Z
+  # TODO: checks for other input arguments
+  # E.g., 'center' should only be TRUE if 'QR' and 'whiten' are both FALSE
+
+  # obtain first scatter matrix or convert to class "ICS_scatter"
+  if (is.function(S1)) {
+    S1.X <- get_scatter(X, fun = S1, args = S1_args, label = S1_label)
+  } else S1.X <- to_ICS_scatter(S1, label = S1_label)
+  # update label for first scatter matrix
+  S1_label <- S1.X$label
+
+  # # compute S2_Y
+  # S2_Y <- S2_Y(X, S1.X = S1.X, S2 = S2, S2_args = S2_args,
+  #              QR = QR, whiten = whiten, S1_label, S2_label, ... )
+  # X_Z <- S2_Y$X_Z
+
+  # TODO: move computation of S2_Y back to ICS() instead of separate function
+  if (is.function(S2)) {
+
+    # Initialization
+    # TODO: check if this can be avoided
+    S2.X <- S2
+
+    if (QR) {
+
+      # Details
+      n <- nrow(X)
+      p <- ncol(X)
+
+      # perform algorithm based on QR decomposition
+      # centering by the mean
+      X.c = sweep(X, 2, colMeans(X), "-")
+
+      # Permutation by rows
+      # decreasing by infinity norm:  absolute maximum
+      norm_inf <- apply(X.c, 1, function(x) max(abs(x)))
+      order_rows <- order(norm_inf, decreasing = TRUE)
+      X_row_per <- X.c[order_rows,]
+
+      # QR decomposition of X with column pivoting from LAPACK
+      qr_X <- qr( 1/sqrt(n-1)*X_row_per, LAPACK = TRUE)
+
+      # R should be pxp
+      R <- qr.R(qr_X)
+
+      # Q should be nxp
+      Q <- qr.Q(qr_X)
+
+      # computation of d_i
+      d_i <- (n-1)*apply(Q, 1, function(x) sum(x^2))
+
+      # Spectral decomposition (SEP) of S2.Y
+      # Computation of S2 - with general one-step M-estimators
+      S2.Y <- S2_args$cf*1/n*(n-1)*t(Q) %*% diag(d_i^S2_args$alpha) %*% Q
+      # convert scatter matrices to class "ICS_scatter"
+      S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
+
+      # Reorder the rows and cols of X
+      X_Z <-  X_row_per[order(order_rows),qr_X$pivot]
+
+      # TODO: I don't think that this is necessary
+      # B1 <- NULL
+
+    } else {
+
+      # compute B1 = S1.X^-1/2
+      S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
+      B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
+
+      # obtain second scatter matrix and whiten the data if requested
+      # convert scatter matrices to class "ICS_scatter"
+      if (whiten) {
+        Y <- X %*% B1
+        S2.Y <- get_scatter(Y, fun = S2, args = S2_args, label = S2_label)
+      } else {
+        # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
+        S2.X <- get_scatter(X, fun = S2, args = S2_args)
+        S2.Y <- to_ICS_scatter(B1 %*% S2.X$scatter %*% B1, label = S2_label)
+      }
+
+      # copy data matrix
+      # TODO: check if this can be avoided
+      X_Z <- X
+
+      # TODO: I don't think that this is necessary
+      # R <- NULL
+
+    }
+
+  } else {
+
+    # perform 1st step of ICS
+    # compute B1 = S1.X^-1/2
+    S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
+    B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
+
+    # obtain second scatter matrix
+    # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
+    # convert scatter matrices to class "ICS_scatter"
+    S2.X <- to_ICS_scatter(S2, label = S2_label)
+    S2.Y <- B1 %*% S2.X$scatter %*% B1
+    S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
+
+    # copy data matrix
+    # TODO: check if this can be avoided
+    X_Z <- X
+
+  }
 
   # decomposition of S2
-  S2.Y.eigen <- eigen(S2_Y$S2.Y$scatter, symmetric=TRUE)
+  S2.Y.eigen <- eigen(S2.Y$scatter, symmetric=TRUE)
   U2 <- S2.Y.eigen$vectors
   lambda <- S2.Y.eigen$values
   if (QR) {
     # Eigenvectors by rows
-    Rinv <- qr.solve(S2_Y$R)
+    Rinv <- qr.solve(R)
     B <- t(U2) %*% t(Rinv)
 
   } else {
-    B <- crossprod(U2, S2_Y$B1)
+    B <- crossprod(U2, B1)
   }
 
   # choosing the signs of B
@@ -259,18 +367,19 @@ ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
   center <- isTRUE(center)
   if (center){
     if (is.function(S2)) {
-      # TODO: Is this correct? If S2 is a function, we have S2.X in most cases
-      #       so that we can check if we have a location component, no? As far
-      #       as I can see, we only don't have a location component if the QR
-      #       algorithm has been used. Am I missing something?
-      # Aurore: for example TCOV is a function but has no location component
+      # TODO: The first part is unnecessary with checks for argument 'center'
+      #       (see also TODO comment below). If S2 is a function, we have S2.X
+      #       in most cases so that we can check if we have a location
+      #       component. As far as I can see, we only don't have an
+      #       "ICS_scatter" object S2_X if the QR algorithm or whitening have
+      #       been used.
       if (is.null(S1.X$location) && is.function(S2)) {
         center <- FALSE
         warning("'S1' needs to have a location component for centering ",
                 "the data; proceeding without centering")
       }
     } else {
-      if (is.null(S1.X$location) || is.null(S2_Y$S2.X$location)) {
+      if (is.null(S1.X$location) || is.null(S2.X$location)) {
         center <- FALSE
         warning("'S1' and 'S2' need to have a location component for ",
                 "centering the data; proceeding without centering")
@@ -282,6 +391,9 @@ ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
   # TODO: add more comments for this next part
   if (center) {
 
+    # TODO: with proper checks for argument 'center', this part can be removed
+    #       (i.e., 'center' is only applicable when we don't have QR algorithm
+    #       or whitening, which can be checked before computing S2_Y)
     if (is.function(S2)) {
       S2.X <- get_scatter(X, fun = S2, args = S2_args)
     } else {

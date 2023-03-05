2d56e34ed2a81f872dddd0ca7ac1dfd05c05d585
Andreas Alfons <alfons@ese.eur.nl>
2023-03-05 13:50:25 +0100

    cleaned up code for QR algorithm but not yet for other algorighms (hence unit tests produce ERROR, will fix later)

--- R/ICS_S3.R
+++ R/ICS_S3.R
@@ -1,3 +1,4 @@
+# Scatter functions returning class "ICS_scatter" -----
 
 #' Title
 #'
@@ -55,12 +56,15 @@ ICS_cov4 <- function(x, location = c("mean3", "mean", "none")) {
 ICS_covW <- function(x, na.action = na.fail, alpha = 1, cf = 1) {
   # compute center and scatter estimates
   scatter <- covW(x, na.action = na.fail, alpha = alpha, cf = cf)
-  out <- list(location = colMeans(x), scatter = scatter, label = "COVW")
+  out <- list(location = colMeans(x), scatter = scatter,
+              label = get_covW_label())
   # add class and return object
   class(out) <- "ICS_scatter"
   out
 }
 
+
+# Main function to compute ICS -----
 
 #' Title
 #'
@@ -152,147 +156,107 @@ ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
 
   # obtain first scatter matrix
   if (is.function(S1)) {
-    S1.X <- get_scatter(X, fun = S1, args = S1_args, label = S1_label)
-  } else S1.X <- to_ICS_scatter(S1, label = S1_label)
+    S1_X <- get_scatter(X, fun = S1, args = S1_args, label = S1_label)
+  } else S1_X <- to_ICS_scatter(S1, label = S1_label)
   # update label for first scatter matrix
-  S1_label <- S1.X$label
+  S1_label <- S1_X$label
 
   # obtain second scatter matrix
-  # FIXME: make sure that S2_label is correctly updated (where appropriate)
-  if (is.function(S2)) {
+  if (QR) {
 
-    # Initialization
-    # TODO: check if this can be avoided
-    S2.X <- S2
+    ## perform numerically stable algorithm based on QR decomposition (second
+    ## scatter matrix is specified as a function for a one-step M-estimator)
 
-    if (QR) {
+    # further initializations
+    n <- nrow(X)
 
-      # Details
-      n <- nrow(X)
-      p <- ncol(X)
+    # center the columns of data matrix by the mean
+    X_centered = sweep(X, 2L, colMeans(X), "-")
 
-      # perform algorithm based on QR decomposition
-      # centering by the mean
-      X.c = sweep(X, 2, colMeans(X), "-")
+    # reorder rows by decreasing by infinity norm (maximum in absolute value)
+    norm_inf <- apply(abs(X_centered), 1L, max)
+    order_rows <- order(norm_inf, decreasing = TRUE)
+    X_reordered <- X_centered[order_rows, ]
 
-      # Permutation by rows
-      # decreasing by infinity norm:  absolute maximum
-      norm_inf <- apply(X.c, 1, function(x) max(abs(x)))
-      order_rows <- order(norm_inf, decreasing = TRUE)
-      X_row_per <- X.c[order_rows,]
+    # compute QR decomposition with column pivoting from LAPACK
+    qr_X <- qr(X_reordered / sqrt(n-1), LAPACK = TRUE)
 
-      # QR decomposition of X with column pivoting from LAPACK
-      qr_X <- qr( 1/sqrt(n-1)*X_row_per, LAPACK = TRUE)
+    # extract components of the QR decomposition
+    Q <- qr.Q(qr_X)  # should be nxp
+    R <- qr.R(qr_X)  # should be pxp
 
-      # R should be pxp
-      R <- qr.R(qr_X)
+    # compute squared Mahalanobis distances (leverage scores)
+    d <- (n-1) * rowSums(Q^2)
 
-      # Q should be nxp
-      Q <- qr.Q(qr_X)
+    # extract relevant arguments for one-step M-estimator
+    cf <- S2_args$cf
+    if (is.null(cf)) cf <- formals(covW)$cf
+    alpha <- S2_args$alpha
+    if (is.null(alpha)) alpha <- formals(covW)$alpha
 
-      # computation of d_i
-      d_i <- (n-1)*apply(Q, 1, function(x) sum(x^2))
+    # compute the second scatter matrix: this works for one-step M-estimators
+    S2_Y <- cf*(n-1)/n * crossprod(sweep(Q, 1L, d^alpha, "*"), Q)
 
-      # Spectral decomposition (SEP) of S2.Y
-      # Computation of S2 - with general one-step M-estimators
-      S2.Y <- S2_args$cf*1/n*(n-1)*t(Q) %*% diag(d_i^S2_args$alpha) %*% Q
-      # convert scatter matrices to class "ICS_scatter"
-      S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
+    # convert second scatter matrix to class "ICS_scatter" and extract label
+    S2_Y <- to_ICS_scatter(S2_Y, label = get_covW_label())
+    S2_label <- S2_Y$label
 
-      # Reorder the rows and cols of X
-      X_Z <-  X_row_per[order(order_rows),qr_X$pivot]
+    # reorder columns of the centered data matrix
+    X_centered <-  X_centered[, qr_X$pivot]
 
-      # TODO: I don't think that this is necessary
-      # B1 <- NULL
+  } else if (is.function(S2)) {
 
+    ## compute second scatter matrix: if requested, first whiten the data
+    ## with respect to the first scatter matrix
+
+    # compute B1 = S1_X^-1/2
+    S1_X.eigen <- eigen(S1_X$scatter, symmetric=TRUE)
+    B1 <- S1_X.eigen$vectors %*% tcrossprod(diag(S1_X.eigen$values^(-0.5)), S1_X.eigen$vectors)
+
+    # obtain second scatter matrix and whiten the data if requested
+    # convert scatter matrices to class "ICS_scatter"
+    if (whiten) {
+      Y <- X %*% B1
+      S2_Y <- get_scatter(Y, fun = S2, args = S2_args, label = S2_label)
     } else {
-
-      # compute B1 = S1.X^-1/2
-      S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
-      B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
-
-      # obtain second scatter matrix and whiten the data if requested
-      # convert scatter matrices to class "ICS_scatter"
-      if (whiten) {
-        Y <- X %*% B1
-        S2.Y <- get_scatter(Y, fun = S2, args = S2_args, label = S2_label)
-      } else {
-        # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
-        S2.X <- get_scatter(X, fun = S2, args = S2_args)
-        S2.Y <- to_ICS_scatter(B1 %*% S2.X$scatter %*% B1, label = S2_label)
-      }
-
-      # copy data matrix
-      # TODO: check if this can be avoided
-      X_Z <- X
-
-      # TODO: I don't think that this is necessary
-      # R <- NULL
-
+      # or compute S1_X^-1/2*S2_Y*S1_X^-1/2
+      S2.X <- get_scatter(X, fun = S2, args = S2_args)
+      S2_Y <- to_ICS_scatter(B1 %*% S2.X$scatter %*% B1, label = S2_label)
     }
 
   } else {
 
+    ## second scatter matrix supplied by user
+
     # perform 1st step of ICS
-    # compute B1 = S1.X^-1/2
-    S1.X.eigen <- eigen(S1.X$scatter, symmetric=TRUE)
-    B1 <- S1.X.eigen$vectors %*% tcrossprod(diag(S1.X.eigen$values^(-0.5)), S1.X.eigen$vectors)
+    # compute B1 = S1_X^-1/2
+    S1_X.eigen <- eigen(S1_X$scatter, symmetric=TRUE)
+    B1 <- S1_X.eigen$vectors %*% tcrossprod(diag(S1_X.eigen$values^(-0.5)), S1_X.eigen$vectors)
 
     # obtain second scatter matrix
-    # or compute S1.X^-1/2*S2.Y*S1.X^-1/2
+    # or compute S1_X^-1/2*S2_Y*S1_X^-1/2
     # convert scatter matrices to class "ICS_scatter"
     S2.X <- to_ICS_scatter(S2, label = S2_label)
-    S2.Y <- B1 %*% S2.X$scatter %*% B1
-    S2.Y <- to_ICS_scatter(S2.Y, label = S2_label)
-
-    # copy data matrix
-    # TODO: check if this can be avoided
-    X_Z <- X
+    S2_Y <- B1 %*% S2.X$scatter %*% B1
+    S2_Y <- to_ICS_scatter(S2_Y, label = S2_label)
 
   }
 
-  # decomposition of S2
-  S2.Y.eigen <- eigen(S2.Y$scatter, symmetric=TRUE)
-  U2 <- S2.Y.eigen$vectors
-  lambda <- S2.Y.eigen$values
-  if (QR) {
-    # Eigenvectors by rows
-    Rinv <- qr.solve(R)
-    B <- t(U2) %*% t(Rinv)
-
-  } else {
-    B <- crossprod(U2, B1)
-  }
+  # compute eigendecomposition of second scatter matrix
+  S2_Y_eigen <- eigen(S2_Y$scatter, symmetric = TRUE)
+  lambda <- S2_Y_eigen$values
+  if (QR) W <- t(qr.solve(R, S2_Y_eigen$vectors))
+  else W <- crossprod(S2_Y_eigen$vectors, B1)
 
   # centering only makes sense if the scatter objects have location components
-  if (center && (is.null(S1.X$location) || is.null(S2.X$location))) {
+  if (center && (is.null(S1_X$location) || is.null(S2.X$location))) {
     warning("location component in 'S1' and 'S2' required for centering ",
             "the data; proceeding without centering")
     center <- FALSE
   }
 
-  # choosing the signs of B
-  # TODO: add more comments for this next part
+  # scale generalized kurtosis values and fix signs in the 'W' matrix
   if (center) {
-
-    # Andreas: The next part was only needed when 'whiten' and 'center' were
-    # both TRUE. The new argument checks above prevent centering when whitening
-    # is used, so this part is commented out.
-    # if (is.function(S2)) {
-    #   S2.X <- get_scatter(X, fun = S2, args = S2_args)
-    # } else {
-    #   S2.X <- to_ICS_scatter(S2, label = deparse(substitute(S2)))
-    # }
-
-    T1.Z <- S1.X$location %*% B
-    T2.Z <- S2.X$location %*% B
-
-    gamma <- T1.Z - T2.Z
-    skew.signs <- ifelse(gamma >= 0, 1, -1)
-    gamma <- as.vector(skew.signs*gamma)
-
-    B.res <- sweep(B, 1, skew.signs, "*")
-    X_Z <- sweep(X_Z, 2, S1.X$location, "-")
 
     # overrride values of related arguments
     # TODO: Would it make sense to allow scaling the generalized kurtosis
@@ -312,35 +276,54 @@ ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
       fix_signs <- "scores"
     }
 
+    # compute location estimates of the scores
+    T1_Z <- S1_X$location %*% W
+    T2_Z <- S2.X$location %*% W
+    # compute generalized skewness values and signs of each component
+    gamma <- T1_Z - T2_Z
+    skewness_signs <- ifelse(gamma >= 0, 1, -1)
+    # fix signs in 'W' matrix so that generalized skewness values are positive
+    gamma <- as.vector(skewness_signs * gamma)
+    W_final <- sweep(W, 1L, skewness_signs, "*")
+    # center the columns of data matrix by the first location estimate
+    X_centered <- sweep(X, 2, S1_X$location, "-")
+
   } else {
 
-    if (scale_lambda) lambda <- lambda/prod(lambda)^(1/p)
+    # if requested, scale generalized kurtosis values
+    if (scale_lambda) lambda <- lambda / prod(lambda)^(1/p)
+
+    # fix the signs in the 'W' matrix of coefficients
     if (fix_signs == "scores") {
+      # compute scores for initial 'W' matrix of coefficients
+      Z <- if (QR) tcrossprod(X_centered, W) else tcrossprod(X, W)
+      # compute skewness values and signs of each component
+      gamma <- colMeans(Z) - apply(Z, 2L, median)
+      skewness_signs <- ifelse(gamma > 0, 1, -1)
       # fix signs in 'W' matrix so that all components are right-skewed
-      Z1 <- tcrossprod(X_Z, B)
-      gamma <- colMeans(Z1) - apply(Z1, 2, median)
-      skew.signs <- ifelse(gamma > 0, 1, -1)
-      gamma <- as.vector(skew.signs*gamma)
-      B.res <- sweep(B, 1, skew.signs, "*")
+      gamma <- as.vector(skewness_signs * gamma)
+      W_final <- sweep(W, 1L, skewness_signs, "*")
     } else {
       # fix signs in 'W' matrix so that the maximum element per row is positive
       # and that each row has norm 1
-      row.signs <- apply(B, 1, .sign.max)
-      row.norms <- sqrt(rowSums((B)^2))
-      B.res <- sweep(B, 1, row.norms * row.signs, "/")
+      row_signs <- apply(W, 1L, .sign.max)
+      row_norms <- sqrt(rowSums(W^2))
+      W_final <- sweep(W, 1L, row_norms * row_signs, "/")
+      # we don't have (generalized) skewness values in this case
       gamma <- NULL
     }
 
   }
 
   # compute the scores
-  Z <- tcrossprod(X_Z, B.res)
-  colnames(Z) <- paste(rep("IC", p), 1:p, sep = ".")
+  if (QR || center) Z_final <- tcrossprod(X_centered, W_final)
+  else Z_final <- tcrossprod(X, W_final)
+  colnames(Z_final) <- paste("IC", 1:p, sep = ".")
 
   # construct object to be returned
   res <- list(lambda = lambda,
-              W = B.res,
-              scores = Z,
+              W = W_final,
+              scores = Z_final,
               gamma = gamma,
               S1_label = S1_label,
               S2_label = S2_label,
@@ -357,7 +340,7 @@ ICS <- function(X, S1 = ICS_cov, S2 = ICS_cov4, S1_args = list(),
 }
 
 
-
+# Methods for class "ICS" -----
 
 #' @method summary ICS
 #' @export
@@ -423,9 +406,10 @@ print.ICS <- function(object, digits = 4){
 }
 
 
+# Internal functions -----
 
-## internal function to convert scatter matrices to the required class
-## internal function to apply a scatter function to the data matrix
+
+## apply a scatter function to the data matrix
 # X ......... data matrix
 # fun ....... function to compute a scatter matrix
 # args ...... list of additional arguments to be passed to the function
@@ -448,6 +432,7 @@ get_scatter <- function(X, fun = cov, args = list(), convert = TRUE, label) {
 }
 
 
+## convert scatter matrices to the required class
 
 to_ICS_scatter <- function(object, ...) UseMethod("to_ICS_scatter")
 
@@ -480,3 +465,20 @@ to_ICS_scatter.list <- function(object, label) {
 }
 
 to_ICS_scatter.ICS_scatter <- function(object, ...) object
+
+
+# compute the matrix square root or the inverse thereof
+# TODO: there is already an internal function mat.sqrt() in the package, but
+#       that one doesn't allow to compute the inverse
+mat_sqrt <- function(A, inv = FALSE) {
+  # initializations
+  power <- if (inv) -0.5 else 0.5
+  # compute eigendecomposition
+  eigen_A <- eigen(A)
+  # compute matrix square root or inverse
+  eigen_A$vectors %*% tcrossprod(diag(eigen_A$values^power), eigen_A$vectors)
+}
+
+# in QR algorithm, the function for the second scatter matrix is not actually
+# applied, so we need a different way to get the label for covW()
+get_covW_label <- function() "COVW"
